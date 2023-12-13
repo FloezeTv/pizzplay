4b68cc4b314740a84f5841f7dc3d3d6ddcc9da8b
FloezeTv <floeze@floeze.tv>
2023-12-13 21:57:50 +0100

    Made server actually start HTTP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, Router};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+
+#[tokio::main]
+async fn main() {
+    let routes = Router::new().fallback(|| async { (StatusCode::NOT_FOUND, "Not Found") });
+
+    let addr = &SocketAddr::new(IpAddr::from(Ipv6Addr::UNSPECIFIED), 8080);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, routes).await.unwrap();
 }
